145517c243806441546858f35aa01ecfca951984
David S D <sddavid.johan@gmail.com>
2021-08-29 09:17:29 +0530

    Added sender log

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ io.on("connection", (socket) => {
   console.log(`User: ${socket.id} connected`);
 
   socket.on("change", async ({ row, column, text, sheetId, sender }) => {
+    console.log(`${sender} sent ${text} to S${sheetId}R${row}C${column}`);
     io.emit(sheetId, { row, column, text, sender });
   });
 });
